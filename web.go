c4dc268b29849fbbf9aef691f5e9b1583b7dcb81
Arseny <spufru@gmail.com>
2016-10-27 00:00:02 +0300

    sort headers

--- web.go
+++ web.go
@@ -16,8 +16,13 @@ func handler(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(res, "%s\n\n", req.RemoteAddr)
 	fmt.Fprintf(res, "%s %s %s\n", req.Method, req.RequestURI, req.Proto)
 	fmt.Fprintf(res, "Host: %s\n", req.Host)
-	for name, values := range req.Header {
-		for _, value := range values {
+	var names []string
+	for name := range req.Header {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+	for _, name := range names {
+		for _, value := range req.Header[name] {
 			fmt.Fprintf(res, "%s: %s\n", name, value)
 		}
 	}
