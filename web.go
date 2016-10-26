5dd4d8b2f0a03f8f7dfc7aee6735e11e1bf19188
Arseny <spufru@gmail.com>
2016-10-26 23:42:11 +0300

    payload

--- web.go
+++ web.go
@@ -1,14 +1,24 @@
 package web
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+func handler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/plain")
+	res.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+
+	fmt.Fprintf(res, "%s\n\n", req.RemoteAddr)
+	fmt.Fprintf(res, "%s %s %s\n", req.Method, req.RequestURI, req.Proto)
+	fmt.Fprintf(res, "Host: %s\n", req.Host)
+	for name, values := range req.Header {
+		for _, value := range values {
+			fmt.Fprintf(res, "%s: %s\n", name, value)
+		}
+	}
 }
