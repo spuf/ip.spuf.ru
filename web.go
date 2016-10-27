658145e652f28fe36386a1deb3c150802c6d5e53
Arseny <spufru@gmail.com>
2016-10-27 11:47:29 +0300

    hide some headers

--- web.go
+++ web.go
@@ -6,6 +6,12 @@ import (
 	"sort"
 )
 
+var hiddenHeaders = map[string]bool{
+	"X-Cloud-Trace-Context":  true,
+	"X-Google-Apps-Metadata": true,
+	"X-Zoo":                  true,
+}
+
 func init() {
 	http.HandleFunc("/", handler)
 }
@@ -15,10 +21,12 @@ func handler(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 
 	fmt.Fprintf(res, "%s\n\n", req.RemoteAddr)
-	fmt.Fprintf(res, "%s %s %s\n", req.Method, req.RequestURI, req.Proto)
+	fmt.Fprintf(res, "%s %v %s\n", req.Method, req.URL, req.Proto)
 	var names []string
 	for name := range req.Header {
-		names = append(names, name)
+		if _, ok := hiddenHeaders[name]; !ok {
+			names = append(names, name)
+		}
 	}
 	sort.Strings(names)
 	for _, name := range names {
