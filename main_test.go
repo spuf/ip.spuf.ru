ba035ee4d7fe3f7ae5818c86701380b018b6ba71
Arseny Razin <spufru@gmail.com>
2020-03-08 16:57:45 +0800

    add fix tests

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/spuf/ip.spuf.ru/request_dumper"
@@ -53,7 +54,8 @@ func TestTextHtml(t *testing.T) {
 
 	resBody, _ := ioutil.ReadAll(res.Body)
 	resBodyStr := string(resBody)
-	if resBodyStr != "<!doctype html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"utf-8\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n    <style>\n        body {\n            background-color: white;\n            color: black;\n        }\n        @media screen and (prefers-color-scheme: dark) {\n            body{\n                background-color: black;\n                color: white;\n            }\n        }\n        pre {\n            word-wrap: break-word;\n            white-space: pre-wrap;\n        }\n    </style>\n    <title>ip.spuf.ru</title>\n</head>\n<body>\n<pre>192.0.2.1:1234\n\nGET / HTTP/1.1\r\nHost: example.com\r\nAccept: text/html\r\n\r\n</pre>\n</body>\n</html>\n" {
+
+	if strings.Contains(resBodyStr, "<!doctype html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"utf-8\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n    <style>\n        body {\n            background-color: white;\n            color: black;\n        }\n        @media screen and (prefers-color-scheme: dark) {\n            body{\n                background-color: black;\n                color: white;\n            }\n        }\n        pre {\n            word-wrap: break-word;\n            white-space: pre-wrap;\n        }\n    </style>\n    <title>ip.spuf.ru</title>\n</head>\n<body>\n<pre>") && strings.Contains(resBodyStr, "192.0.2.1:1234\n\nGET / HTTP/1.1\r\nHost: example.com\r\nAccept: text/html\r\n\r\n") && strings.Contains(resBodyStr, "</body>\n</html>\n") {
 		t.Fatalf("unexpected body: %#v", resBodyStr)
 	}
 }
